dfe89e03af66201d38cfad657323b0fb2f2f97a1
Robin Wieruch <wrobin@gmx.net>
2017-12-07 17:43:19 +0800

    use route constants

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import SignOutButton from '../SignOut';
+import * as routes from '../../constants/routes';
 
 const Navigation = ({ authUser }) =>
   <div>
@@ -14,16 +15,16 @@ const Navigation = ({ authUser }) =>
 
 const NavigationAuth = () =>
   <ul>
-    <li><Link to="/">Landing</Link></li>
-    <li><Link to="/home">Home</Link></li>
-    <li><Link to="/account">Account</Link></li>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.HOME}>Home</Link></li>
+    <li><Link to={routes.ACCOUNT}>Account</Link></li>
     <li><SignOutButton /></li>
   </ul>
 
 const NavigationNonAuth = () =>
   <ul>
-    <li><Link to="/">Landing</Link></li>
-    <li><Link to="/signin">Sign In</Link></li>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
   </ul>
 
 const mapStateToProps = (state) => ({
